967b33236dfd30bea9c1bef8d60f2a2c90d592c1
FranciumEX1 <44691293+FranciumEX1@users.noreply.github.com>
2019-04-13 04:31:31 +0100

    Add files via upload

---
+++ otherCardDetails.lua
@@ -0,0 +1,38 @@
+local dbase = require("sqlite3")
+
+local otherStats = {}
+
+function otherStats.doesSTExist(cardName)
+    conn = dbase.open("YuGiscord.db")
+    local search = conn:exec("SELECT COUNT(*) FROM SpellsAndTraps WHERE Cardname = '" .. cardName .. "'")
+    --search:reset():bind(cardName):step()
+    local result = tonumber(search[1][1])
+    if result ~= 0 then return true else return false end
+    conn:close()
+end
+
+function otherStats.getName(cardName)
+    local conn = dbase.open("YuGiscord.db")
+    local searcher = conn:exec("SELECT Cardname FROM SpellsAndTraps WHERE Cardname = '" .. cardName .. "'")
+    local result = searcher[1][1]
+    conn:close()
+    return result
+end
+
+function otherStats.getDescription(cardName)
+    local conn = dbase.open("YuGiscord.db")
+    local searcher = conn:exec("SELECT Description FROM SpellsAndTraps WHERE Cardname = '" .. cardName .. "'")
+    local result = searcher[1][1]
+    conn:close()
+    return result
+end
+
+function otherStats.getImage(cardName)
+    local conn = dbase.open("YuGiscord.db")
+    local searcher = conn:exec("SELECT CardImage FROM SpellsAndTraps WHERE Cardname = '" .. cardName .. "'")
+    local result = searcher[1][1]
+    conn:close()
+    return result
+end
+
+return otherStats
