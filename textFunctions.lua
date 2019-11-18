cc503185fe07fadc4a8fd8b7c6d3d3932bdd8fcc
FranciumEX1 <44691293+FranciumEX1@users.noreply.github.com>
2019-11-18 20:21:12 +0000

    Delete textFunctions.lua

--- textFunctions.lua
+++
@@ -1,8 +0,0 @@
-stringStuff = {}
-
-function stringStuff.stringConverter(cardName)
-    local newString = string.gsub(" " .. cardName, "%W%l", string.upper):sub(2)
-    return newString
-end
-
-return stringStuff
